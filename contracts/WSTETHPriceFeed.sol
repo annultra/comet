604e6fe115f419dbdff2cc5cf705d270c0f46e38
Scott Silver <scott@compound.finance>
2022-10-13 14:38:58 -0700

    WIP 1

--- contracts/WSTETHPriceFeed.sol
+++ contracts/WSTETHPriceFeed.sol
@@ -3,20 +3,44 @@ pragma solidity 0.8.15;
 
 import "./vendor/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+interface IWstETH {
+    function decimals() external view returns (uint8);
+    function tokensPerStEth() external view returns (uint256);
+}
+
 contract WSTETHPriceFeed is AggregatorV3Interface {
+    /** Custom errors **/
+    error InvalidInt256();
+    error NotImplemented();
+
+    /// @notice
     uint8 public override decimals = 8;
 
-    string public constant override description = "Custom price feed for wsETH / USD";
+    /// @notice
+    string public constant override description = "Custom price feed for wstETH / USD";
 
+    /// @notice
     uint public constant override version = 1;
 
-    int256 public price = 7;
+    /// @notice
+    address public stETHtoUSDPriceFeed = 0xCfE54B5cD566aB89272946F602D76Ea879CAb4a8;
 
-    constructor(int initialPrice, uint8 decimals_) {
-        // price feed address
-        // wsteth address
+    /// @notice
+    address public wstETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+
+    /**
+     * @notice
+     * @param
+     **/
+    function signed256(uint256 n) internal pure returns (int256) {
+        if (n > uint256(type(int256).max)) revert InvalidInt256();
+        return int256(n);
     }
 
+    /**
+     * @notice
+     * @param
+     **/
     function getRoundData(uint80 _roundId) override external view returns (
         uint80 roundId,
         int256 answer,
@@ -24,10 +48,18 @@ contract WSTETHPriceFeed is AggregatorV3Interface {
         uint256 updatedAt,
         uint80 answeredInRound
     ) {
-        // throw error?
-        return (_roundId, price, 0, 0, 0);
+        revert NotImplemented();
     }
 
+    /**
+     * @notice
+     * @param
+     * @return roundId Always 0
+     * @return answer XXX
+     * @return startedAt Always 0
+     * @return updatedAt Always 0
+     * @return answeredInRound Always 0
+     **/
     function latestRoundData() override external view returns (
         uint80 roundId,
         int256 answer,
@@ -35,6 +67,10 @@ contract WSTETHPriceFeed is AggregatorV3Interface {
         uint256 updatedAt,
         uint80 answeredInRound
     ) {
+        (,int256 stETHPrice, , , ) = AggregatorV3Interface(stETHtoUSDPriceFeed).latestRoundData();
+        uint8 wstDecimals = IWstETH(wstETH).decimals();
+        uint256 tokensPerStEth = IWstETH(wstETH).tokensPerStEth();
+        int price = stETHPrice * signed256(tokensPerStEth) / signed256(10 ** wstDecimals);
         return (0, price, 0, 0, 0);
     }
 }
--- test/wsteth-price-feed.ts
+++ test/wsteth-price-feed.ts
@@ -1,8 +1,52 @@
-import { baseBalanceOf, ethers, expect, exp, makeProtocol, wait, makeBulker, defaultAssets, getGasUsed, makeRewards, fastForward } from './helpers';
-import { FaucetWETH__factory } from '../build/types';
+import hre, { ethers } from 'hardhat';
+import { expect } from './helpers';
+import { HttpNetworkConfig } from 'hardhat/types/config';
+import {
+  WSTETHPriceFeed,
+  WSTETHPriceFeed__factory
+} from '../build/types';
+
+export async function forkMainnet() {
+  const mainnetConfig = hre.config.networks.mainnet as HttpNetworkConfig;
+  await ethers.provider.send(
+    'hardhat_reset',
+    [
+      {
+        forking: {
+          jsonRpcUrl: mainnetConfig.url,
+        },
+      },
+    ],
+  );
+}
+
+export async function resetHardhatNetwork() {
+  // reset to blank hardhat network
+  await ethers.provider.send('hardhat_reset', []);
+}
+
+
+export async function makeWstETH() {
+  const wstETHPriceFeedFactory = (await ethers.getContractFactory('WSTETHPriceFeed')) as WSTETHPriceFeed__factory;
+  const wstETHPriceFeed = await wstETHPriceFeedFactory.deploy();
+  await wstETHPriceFeed.deployed();
+
+  return {
+    wstETHPriceFeed
+  };
+}
 
 describe.only('wstETH price feed', function () {
+  before(forkMainnet);
+  after(resetHardhatNetwork);
+
   it('tests something', async () => {
+    const { wstETHPriceFeed } = await makeWstETH();
+    const latestRoundData = await wstETHPriceFeed.latestRoundData();
+
+    console.log(latestRoundData.answer.toBigInt());
+
     expect(true).to.be.true;
   });
+
 });
